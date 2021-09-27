48beaa06698051d2b1e4d4576fbabcc46959099b
DaeHyeonHong <fholng@outlook.com>
2021-09-27 23:53:59 +0900

    [1강]
    using을 이용한 명칭공간 사용

--- FirstStep/FirstStep.cpp
+++ FirstStep/FirstStep.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
-int main() 
-{
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "나의 첫 번째 c++ 프로그램" << std::endl;
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
     return 0;
 }
